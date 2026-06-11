72adef7598992c9559122b355066101d7c9ef80d
Chaiwutz14 <peeza1482546@gmail.com>
2026-06-11 11:48:05 +0700

    Delete js directory

--- js/theme-init.js
+++
@@ -1,5 +0,0 @@
-/* Diamond Cute Studio — Theme Init (runs before CSS) */
-(function(){
-  var t = localStorage.getItem('dmc_theme') || 'sky';
-  document.documentElement.setAttribute('data-theme', t);
-})();
